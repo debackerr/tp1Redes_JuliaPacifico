1909cf5c8d2e7a020de48565ef8530ebd532b832
Júlia de Backer <55795526+debackerr@users.noreply.github.com>
2021-06-30 21:28:34 -0300

    Create servidor.c

---
+++ servidor.c
@@ -0,0 +1,95 @@
+#include "common.h"
+#include "handles.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define BUFSZ 500
+
+void usage(int argc, char **argv) {
+    printf("usage: %s <v4|v6> <server port>\n", argv[0]);
+    printf("example: %s v4 51511\n", argv[0]);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        usage(argc, argv);
+    }
+
+    struct sockaddr_storage storage;
+    if (0 != server_sockaddr_init(argv[1], argv[2], &storage)) {
+        usage(argc, argv);
+    }
+
+    int s;
+    s = socket(storage.ss_family, SOCK_STREAM, 0);
+    if (s == -1) {
+        logexit("socket");
+    }
+
+    int enable = 1;
+    if (0 != setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int))) {
+        logexit("setsockopt");
+    }
+
+    struct sockaddr *addr = (struct sockaddr *)(&storage);
+    if (0 != bind(s, addr, sizeof(storage))) {
+        logexit("bind");
+    }
+
+    if (0 != listen(s, 10)) {
+        logexit("listen");
+    }
+
+    char addrstr[BUFSZ];
+    addrtostr(addr, addrstr, BUFSZ);
+    printf("bound to %s, waiting connections\n", addrstr);
+
+    int MAIN_DATA[50][2];
+    // MAIN_DATA: data matrix with every location created
+    for( int i = 0; i < 50; i++){
+        for (int j = 0; j < 2; j++){
+            MAIN_DATA[i][j] = -1;
+        }
+    }
+    
+    while (1) {
+        struct sockaddr_storage cstorage;
+        struct sockaddr *caddr = (struct sockaddr *)(&cstorage);
+        socklen_t caddrlen = sizeof(cstorage);
+
+        int csock = accept(s, caddr, &caddrlen);
+        if (csock == -1) {
+            logexit("accept");
+        }
+
+        char caddrstr[BUFSZ];
+        addrtostr(caddr, caddrstr, BUFSZ);
+        printf("[log] connection from %s\n", caddrstr);
+
+        char buf[BUFSZ];
+        memset(buf, 0, BUFSZ);
+        size_t count;
+
+        while(1){
+            memset(buf, 0, BUFSZ);  
+            count = recv(csock, buf, BUFSZ - 1, 0);
+            printf("[msg] %s, %d bytes: %s\n", caddrstr, (int)count, buf);    
+
+            handle(buf, MAIN_DATA);
+            count = send(csock, buf, strlen(buf), 0);
+            if (count != strlen(buf)) {
+                logexit("send");
+            }
+        }
+        close(csock);   
+    }
+
+    exit(EXIT_SUCCESS);
+}
